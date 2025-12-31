fa4748112d6d33c46f9475aa5efd521adacc83e5
panosV <panosVasilopoulos92@gmail.com>
2025-12-31 14:05:13 +0200

    feat(PFA-2): Minor additions to project.

--- src/main/java/org/viators/personal_finance_app/repository/UserRepository.java
+++ src/main/java/org/viators/personal_finance_app/repository/UserRepository.java
@@ -4,7 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.viators.personal_finance_app.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 
 }
