ef5c9ade3188d364d33498de10a52248a6cddd3e
panosV <panosVasilopoulos92@gmail.com>
2026-01-07 18:12:03 +0200

    feat(PFA-2): Creates dto summaries for user relationships in order to use them in userDTOs class.

--- src/main/java/org/viators/personal_finance_app/service/UserService.java
+++ src/main/java/org/viators/personal_finance_app/service/UserService.java
@@ -2,10 +2,10 @@ package org.viators.personal_finance_app.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.viators.personal_finance_app.dtos.UserDTOs;
 import org.viators.personal_finance_app.model.User;
 
 @Service
 @Transactional(readOnly = true)
 public class UserService {
-
 }
